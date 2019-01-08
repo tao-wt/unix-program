701805faf96fedab53282a6e06a14e446bb76201
tao-wt <xx201890@126.com>
2019-01-08 13:47:18 +0800

    和file_hole.c比较用

---
+++ file_no_hole_2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define FILE_MODE       (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+char    buf1[] = "abcdefghij";
+char    buf2[] = "ABCDEFGHIJ";
+char    buf3[] = "-";
+
+void err_sys(char *str)
+{
+        printf("%s", str);
+        exit(3);
+}
+
+int main(void)
+{
+        int             fd;
+
+        if ((fd = creat("file.no_hole_2", FILE_MODE)) < 0)
+                err_sys("creat error");
+
+        if (write(fd, buf1, 10) != 10)
+                err_sys("buf1 write error");
+        while (lseek(fd, 0, SEEK_CUR) < 16384){
+            if (write(fd, buf3, 1) != 1)
+                err_sys("buf1 write error");
+        }
+        /* offset now = 10 */
+
+        //if (lseek(fd, 16384, SEEK_SET) == -1)
+        //        err_sys("lseek error");
+        /* offset now = 16384 */
+        printf("offset:%ld\n", lseek(fd, 0, SEEK_CUR));
+        if (write(fd, buf2, 10) != 10)
+                err_sys("buf2 write error");
+        /* offset now = 16394 */
+
+        exit(0);
+}
