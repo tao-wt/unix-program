7b550a425ccce278681e0797712c87036e0513f7
tao-wt <xx201890@126.com>
2019-01-10 17:33:29 +0800

    获取文件标志

---
+++ fileflags.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void err_sys(char *str)
+{
+        printf("%s\n", str);
+        exit(3);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int		val;
+
+	if (argc != 2)
+		err_sys("usage: a.out <descriptor#>");
+
+	if ((val = fcntl(atoi(argv[1]), F_GETFL, 0)) < 0)
+		err_sys("fcntl error for fd");
+
+	switch (val & O_ACCMODE) {
+	case O_RDONLY:
+		printf("read only");
+		break;
+
+	case O_WRONLY:
+		printf("write only");
+		break;
+
+	case O_RDWR:
+		printf("read write");
+		break;
+
+	default:
+		printf("unknown access mode");
+	}
+
+	if (val & O_APPEND)
+		printf(", append");
+	if (val & O_NONBLOCK)
+		printf(", nonblocking");
+	if (val & O_SYNC)
+		printf(", synchronous writes");
+
+#if !defined(_POSIX_C_SOURCE) && defined(O_FSYNC) && (O_FSYNC != O_SYNC)
+	if (val & O_FSYNC)
+		printf(", synchronous writes");
+#endif
+
+	putchar('\n');
+	exit(0);
+}
